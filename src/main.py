01244a97fbb0b7a69244430445029694dd717a46
RobynIsBlue <tyglivermont@gmail.com>
2025-01-06 08:29:16 -0600

    !!!! done!!

--- src/main.py
+++ src/main.py
@@ -3,22 +3,26 @@ from textnode import *
 import os
 import shutil
 
-def recurring_files_to_dest(from_dir, to_dir):
-    if os.path.isfile(from_dir):
-        print("from: " + str(from_dir))
-        return shutil.copy(from_dir)
-    new_path = os.mkdir(shutil.copy(from_dir))
-    new_path = os.path.join(new_path, from_dir)
-    for file_or_dir in os.listdir(from_dir):
-        recurring_files_to_dest(file_or_dir, to_dir)
+def recurring_files_to_dest(from_x, to_dir):
+    if os.path.isfile(from_x):
+        print("from: " + str(from_x))
+        shutil.copy(from_x, to_dir)
+        return
+    dir_name = from_x.split("/")[-1]
+    joined_paths = os.path.join(to_dir, dir_name)
+    os.mkdir(joined_paths)
+    for file_or_dir in os.listdir(from_x):
+        new_file_or_dir = os.path.join(from_x, file_or_dir)
+        recurring_files_to_dest(new_file_or_dir, joined_paths)
 
 def plus_delete(from_dir, to_dir):
     shutil.rmtree(to_dir)
+    os.mkdir(to_dir)
     recurring_files_to_dest(from_dir, to_dir)
 
 
 def main():
-    plus_delete("../static", "../public")
+    plus_delete("static", "public")
 
 # Using the special variable 
 # __name__
